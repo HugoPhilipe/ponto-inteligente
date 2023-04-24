9ac845d86d0e302110c854795e5908c5c07bd9c4
HugoPhilipe <lipy_hugo@live.com>
2023-04-23 22:26:39 -0300

    Modulo e componente de pessoa física

---
+++ src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CadastrarPfComponent, CadastroPfComponent } from './components';
+
+
+
+@NgModule({
+  declarations: [
+    CadastrarPfComponent,
+    CadastroPfComponent
+  ],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule
+  ]
+})
+export class CadastroPfModule { }
